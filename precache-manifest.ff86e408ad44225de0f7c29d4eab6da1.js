1409004046a9dedb9cd6b27c70af2651d1439cc6
amine-chatteli <amin.chatteli@gmail.com>
2020-06-25 17:35:47 +0200

    Updates

---
+++ precache-manifest.ff86e408ad44225de0f7c29d4eab6da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1fe8b7260e0662b3e6ec89adc1d571",
+    "url": "/guess-country-flag-react/index.html"
+  },
+  {
+    "revision": "75cd4cafb93baae467c8",
+    "url": "/guess-country-flag-react/static/css/main.7f016748.chunk.css"
+  },
+  {
+    "revision": "6872911ea31cba33218d",
+    "url": "/guess-country-flag-react/static/js/2.95fda0f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-country-flag-react/static/js/2.95fda0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cd4cafb93baae467c8",
+    "url": "/guess-country-flag-react/static/js/main.aa2fdacc.chunk.js"
+  },
+  {
+    "revision": "6f51de4f5a18b282d140",
+    "url": "/guess-country-flag-react/static/js/runtime-main.961fd4a8.js"
+  }
+]);
